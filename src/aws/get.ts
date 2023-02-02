41d5628c05502d6d5967d7bf38376935bf4970c1
Lee <44310445+lx200916@users.noreply.github.com>
2023-02-02 17:57:24 +0800

    Support `Byte-Range`

--- src/aws/get.ts
+++ src/aws/get.ts
@@ -5,20 +5,23 @@ import { guessIsFolder } from "../utils";
 async function getFile(
   aws: AwsClient,
   env: Env,
-  path: string
+  path: string,request:Request
 ): Promise<Response> {
-  // return Response.redirect(`${env.BUCKET_BASEURL}/${path}`, 307);
+  // return Response.redirect(`${env.BUCKET_BASEURL}/${path}`, 302);
   const resp = await aws.fetch(`${env.BUCKET_BASEURL}/${path}`, {
     method: "GET",
+    headers: request.headers.get("range")?  {
+      "range":request.headers.get("range")!,
+    }:undefined,
   });
   let { readable, writable } = new TransformStream();
   if (resp.body == null) return new Response(null, { status: 204, headers: { DAV: "1" } });
 
-  // Start pumping the body. NOTE: No await!
   resp.body.pipeTo(writable);
+  let respHeaders = new Headers(resp.headers);
+  respHeaders.set("DAV", "1");
 
-  // ... and deliver our Response while that’s running.
-  return new Response(readable, {headers:{'DAV':'1'},status:resp.status});
+  return new Response(readable, {headers:respHeaders,status:resp.status});
 }
 async function getFolder(aws: AwsClient, env: Env, path: string) {
   return new Response(null, { status: 403 ,headers:{'DAV':'1'}});
@@ -26,6 +29,6 @@ async function getFolder(aws: AwsClient, env: Env, path: string) {
 async function Get(aws: AwsClient, env: Env, request: Request,path:string) {
   return guessIsFolder(path)
     ? await getFolder(aws, env, path)
-    : await getFile(aws, env, path);
+    : await getFile(aws, env, path,request);
 }
 export { Get };
